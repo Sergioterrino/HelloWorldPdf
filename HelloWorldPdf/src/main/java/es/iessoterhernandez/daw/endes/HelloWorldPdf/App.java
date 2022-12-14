c5b3dfcce7ee954379baf1d16c93c882a997d4aa
Sergioterrino <120554805+Sergioterrino@users.noreply.github.com>
2022-12-14 09:34:57 +0100

    Add files via upload

---
+++ HelloWorldPdf/src/main/java/es/iessoterhernandez/daw/endes/HelloWorldPdf/App.java
@@ -0,0 +1,33 @@
+package es.iessoterhernandez.daw.endes.HelloWorldPdf;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class App 
+{
+    public static void main( String[] args ) throws DocumentException
+    {
+    	try {
+            Document document = new Document();
+            String dest = "hello_world.pdf";
+            PdfWriter.getInstance(document, new FileOutputStream(dest));
+            document.open();
+             
+            Phrase header = new Phrase("Hello world, acabo de crear mi primer PDF :) ");
+            document.add(header);
+             
+            document.close();
+             
+            System.out.println("PDF creado");
+             
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
